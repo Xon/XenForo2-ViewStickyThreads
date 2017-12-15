d3687c9e1c32d5afb2649538b1e854bf702ebf92
Xon <tazipper1@hotmail.com>
2017-12-15 09:08:50 +0800

    Store _output not _data

---
+++ upload/src/addons/SV/ViewStickyThreads/_output/extension_hint.php
@@ -0,0 +1,14 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\ViewStickyThreads\XF\Entity
+{
+	class XFCP_Thread extends \XF\Entity\Thread {}
+}
+
+namespace SV\ViewStickyThreads\XF\Finder
+{
+	class XFCP_Thread extends \XF\Finder\Thread {}
+}
