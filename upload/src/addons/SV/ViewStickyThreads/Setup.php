5ad0c68bfb4ff354e1d35ae51b136fa3b06cd9ba
Xon <tazipper1@hotmail.com>
2018-02-21 23:41:46 +0800

    Remove permission rebuild jobs

--- upload/src/addons/SV/ViewStickyThreads/Setup.php
+++ upload/src/addons/SV/ViewStickyThreads/Setup.php
@@ -17,23 +17,11 @@ class Setup extends AbstractSetup
 	public function upgrade2000070Step1()
 	{
 	    $this->db()->query("delete from xf_permission_entry where permission_group_id = 'forum' and permission_id = 'viewStaff'");
-        $this->app->jobManager()->enqueueUnique(
-            'permissionRebuild',
-            'XF:PermissionRebuild',
-            [],
-            false
-        );
 	}
 
     public function uninstallStep1()
     {
         $this->db()->query("delete from xf_permission_entry where permission_group_id = 'forum' and permission_id = 'viewStaff'");
         $this->db()->query("delete from xf_permission_entry where permission_group_id = 'forum' and permission_id = 'viewStickies'");
-        $this->app->jobManager()->enqueueUnique(
-            'permissionRebuild',
-            'XF:PermissionRebuild',
-            [],
-            false
-        );
     }
 }
